85c3dd3941254e8d5515d10a255f12c52dc582ee
yewen <18868100395@163.com>
2023-02-15 16:53:08 +0800

    Add Cors and Test api

--- app.js
+++ app.js
@@ -3,21 +3,26 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
+
 const { format } = require("date-fns");
 
 // 1st party dependencies
 var indexRouter = require("./routes/index");
+var testRouter = require("./routes/testapi");
+
 const { chatCall } = require("./openai");
 
 async function getApp() {
   var app = express();
-  var port = normalizePort(process.env.PORT || '3000');
+  var port = normalizePort(process.env.PORT || '9000');
   app.set('port', port);
 
   // view engine setup
   app.set("views", path.join(__dirname, "views"));
   app.set("view engine", "pug");
 
+  app.use(cors());
   app.use(logger("dev"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
@@ -27,6 +32,7 @@ async function getApp() {
   app.locals.format = format;
 
   app.use("/", indexRouter);
+  app.use("/testapi", testRouter);
   app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js")); // redirect bootstrap JS
   app.use(
     "/css",
