869ea8725e47b8acde6f007faa70496b5eab53c4
yewen <18868100395@163.com>
2023-02-16 11:20:46 +0800

    Add Api

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,18 @@ router.get('/', function(req, res, next) {
     res.render('index');
 });
 
+router.post('/rephrase', function(req, res, next) {
+    const text = req.body.text;
+    console.log(`rephrase text, `, text);
+    res.send({"text": text});
+});
+
+router.post('/translate', function(req, res, next) {
+    const text = req.body.text;
+    const lang = req.body.lang;
+
+    console.log(`rephrase lang, text, `, lang, text);
+    res.send({"text": text});
+});
+
 module.exports = router;
